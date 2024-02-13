2ccae230af2087b99c06ace32d5573cc8eecd773
Bear <ericbearcrump@gmail.com>
2024-02-12 21:41:36 -0600

    Initialize SpringBoot / Java for WavyDaves.Api. Begin controller boilerplate for UsersController.java

---
+++ WavyDaves.Api/src/main/java/com/WavyDaves/WavyDaves/Api/Application.java
@@ -0,0 +1,13 @@
+package com.WavyDaves.WavyDaves.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ WavyDaves.Api/src/main/java/com/WavyDaves/WavyDaves/Api/controllers/UsersController.java
@@ -0,0 +1,7 @@
+package com.WavyDaves.WavyDaves.Api.controllers;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+}
