62fedafeb44c70bae7e808a422d1406df03e0462
Bear Crump <ericbearcrump@gmail.com>
2024-02-18 11:48:39 -0600

    .

--- WavyDaves.Api/src/main/java/com/WavyDaves/Api/controllers/ItemsController.java
+++ WavyDaves.Api/src/main/java/com/WavyDaves/Api/controllers/ItemsController.java
@@ -1,9 +1,25 @@
 package com.WavyDaves.Api.controllers;
 
+import com.WavyDaves.Api.models.Item;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/v1/items")
 public class ItemsController {
+
+    @GetMapping
+    public void getItems() {
+        throw new RuntimeException("Method not Implemented");
+    }
+
+    @PostMapping
+    public void postItem() {
+        throw new RuntimeException("Method not implemented");
+    }
+
 }
